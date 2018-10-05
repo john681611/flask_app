f1cc846eeb2fed87e9c08eb39edc2f5d281e476b
john681611 <john.harvey@marks-and-spencer.com>
2018-10-05 16:29:30 +0100

    inital render

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask("MyApp")
+
+@app.route("/")
+def hello():
+  return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+  return render_template("index.html", name=name.title())
+
+app.run(debug=True)
